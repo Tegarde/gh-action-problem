7950b7222667c1eb81a7efdb97577345b0f07caa
Tegarde <bruno.oliveira.1994@hotmail.com>
2025-12-25 18:43:45 +0000

    Changed s3 deploy action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 
 function run() {
-    core.notice('Hello from my custom action');
+    // 1) Get inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files to bucket
+    const s3Uri = `s3://${bucket}`;
+    core.notice(`Deploying to ${s3Uri} on region ${region} for the folder ${distFolder}`);
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`, { ignoreReturnCode: true });
 }
 
 run();
